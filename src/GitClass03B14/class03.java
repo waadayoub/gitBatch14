113934cfdd5a980f77c27a0d9cee86d588ac8547
waadayoub <waadkayoub@gmail.com>
2022-09-27 18:50:25 -0500

    second commit

--- src/GitClass03B14/class03.java
+++ src/GitClass03B14/class03.java
@@ -5,6 +5,7 @@ public class class03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hey this is a git class");
+System.out.println("hey this is a change in my project");
 	}
 
 }
